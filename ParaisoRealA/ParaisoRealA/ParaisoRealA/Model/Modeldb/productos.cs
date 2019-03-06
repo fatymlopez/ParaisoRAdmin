fa29a0d4f999f3be4b8ee3a248ffa3827e5da664
Fatima <fatymlopez92@gmail.com>
2019-03-06 07:33:30 -0600

    modiadmin

--- ParaisoRealA/ParaisoRealA/ParaisoRealA/Model/Modeldb/productos.cs
+++ ParaisoRealA/ParaisoRealA/ParaisoRealA/Model/Modeldb/productos.cs
@@ -6,11 +6,14 @@ namespace ParaisoRealA.Model.Modeldb
 {
     public class productos
     {
+        
         public int id { get; set; }
         public int idcategoria { get; set; }
         public string nomproducto { get; set; }
         public string descripcion { get; set; }
-        public decimal precio { get; set; }
         public int existencia { get; set; }
+        public decimal precio { get; set; }
+
+        
     }
 }
