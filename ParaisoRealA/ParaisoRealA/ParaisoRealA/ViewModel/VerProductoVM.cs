c3b3f8ce0575971cccb10f0e0516749e237ed330
Fatima <fatymlopez92@gmail.com>
2019-03-11 22:54:20 -0600

    buscar

--- ParaisoRealA/ParaisoRealA/ParaisoRealA/ViewModel/VerProductoVM.cs
+++ ParaisoRealA/ParaisoRealA/ParaisoRealA/ViewModel/VerProductoVM.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace ParaisoRealA.ViewModel
 {
-    public class VerProductoVM
+    public class VerProductoVM 
     {
         public VerProductoVM()
         {
 
 
         }
+
+       
     }
 }
